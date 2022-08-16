887a667ac0484b8eb0c3a0272010c97e15a33878
Kenny Elshoff <kelshoff@grahamdigital.com>
2022-08-16 11:18:48 -0400

    Make `NEXTAUTH_URL` optional on Vercel

--- src/server/env-schema.mjs
+++ src/server/env-schema.mjs
@@ -4,7 +4,10 @@ export const envSchema = z.object({
   DATABASE_URL: z.string().url(),
   NODE_ENV: z.enum(["development", "test", "production"]),
   NEXTAUTH_SECRET: z.string(),
-  NEXTAUTH_URL: z.string().url(),
+  NEXTAUTH_URL:
+    process.env.VERCEL_ENV === "production"
+      ? z.string().url().optional()
+      : z.string().url(),
   GITHUB_CLIENT_ID: z.string(),
   GITHUB_CLIENT_SECRET: z.string(),
   // DISCORD_CLIENT_ID: z.string(),
